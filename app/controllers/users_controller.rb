573ab121fce4f5f9762510d2025f5f5c6868791d
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-12 00:16:43 +0200

    fix error for form field names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,32 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    # @users = User.all
   end
 
   def new
     @user = User.new
   end
 
-  def create 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = Article.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 10 }
-  validates :email, presence: true, length: { minimum: 10 }
-  validates :password, presence: true, length: { minimum: 10 }
+  validates :username, presence: true, length: { minimum: 20 }
+  validates :email, presence: true, length: { minimum: 20 }
+  validates :password, presence: true, length: { minimum: 20 }
 end
