1097dc7a401a6d8dc7869fbee488a68adabd55b9
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-12 07:37:18 +0200

    add edit action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 # main user controller
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def create
