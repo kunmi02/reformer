d01559cc1e7869fc3f149e91b10e3e141ffdecf9
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-12 06:17:15 +0200

    add error message notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # User validations
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 20 }
-  validates :email, presence: true, length: { minimum: 20 }
-  validates :password, presence: true, length: { minimum: 20 }
+  validates :username, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
